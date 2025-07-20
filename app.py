9f2cbfc533e8ac4d7013bb2c971985ac595c310a
ahmed <newl4533@gmail.com>
2025-07-20 13:36:51 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "netgenius"  # نفس التوكن اللي هتكتبه في إعدادات فيسبوك
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Verification failed", 403
+
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run()
